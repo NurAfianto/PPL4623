93ed50f989c8049b70c20db0bf39b3229cb7692c
Yulius Alvin <79980153+yulius37@users.noreply.github.com>
2021-05-30 19:12:59 +0700

    Add files via upload

--- application/controllers/Admin/Kategori.php
+++ application/controllers/Admin/Kategori.php
@@ -1,25 +1,79 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
+defined('BASEPATH') OR exit('no direct script access allowed');
+ 
 class Kategori extends CI_Controller {
-
+	//Koneksi Model
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Kategori_model");
+        $this->load->library('form_validation');
+    }
+	//Read
 	public function index()
 	{
+		$data['Kategori'] = $this->Kategori_model->getAll();
 		$this->load->view('admin/header');
-		$this->load->view('admin/kategori/view_kategori');
+		$this->load->view('admin/kategori/view_kategori',$data);
 		$this->load->view('admin/footer');
 	}
-	
-	public function add(){
+	//Create
+	public function add()
+	{
 		$this->load->view('admin/header');
 		$this->load->view('admin/kategori/view_kategori_add');
 		$this->load->view('admin/footer');
 	}
 
-	public function edit(){
+	public function tambah_aksi()
+	{
+		$this->form_validation->set_rules('nama','Nama','required');
+		if ($this->form_validation->run()==true)
+        {
+			$data['nama'] = $this->input->post('nama');
+			$this->Kategori_model->save($data);
+			redirect('admin/Kategori');
+		}
+		else
+		{
+			$this->load->view('admin/header');
+			$this->load->view('admin/kategori/view_kategori_add');
+			$this->load->view('admin/footer');
+		}
+	}
+	//Update/Edit
+	public function edit($no)
+	{
+		$data['Kategori'] = $this->Kategori_model->getById($no);
 		$this->load->view('admin/header');
-		$this->load->view('admin/kategori/view_kategori_edit');
+		$this->load->view('admin/kategori/view_kategori_edit',$data);
 		$this->load->view('admin/footer');
 	}
-
+	public function update()
+	{
+		$this->form_validation->set_rules('nama','Nama','required');
+		if ($this->form_validation->run()==true)
+        {
+		 	$no = $this->input->post('no');
+			$data['nama'] = $this->input->post('nama');
+			$this->Kategori_model->update($data,$no);
+			redirect('admin/Kategori');
+		}
+		else
+		{
+			$no = $this->input->post('no');
+			$data['Kategori'] = $this->Kategori_model->getById($no);
+			$this->load->view('admin/header');
+			$this->load->view('admin/kategori/view_kategori_edit',$data);
+			$this->load->view('admin/footer');
+		}
+	}
+	//Delete
+	public function delete($no)
+	{
+		$this->Kategori_model->delete($no);
+		redirect('admin/Kategori');
+	}
+	
+	
 }
--- application/controllers/Admin/User.php
+++ application/controllers/Admin/User.php
@@ -1,14 +1,65 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('id direct script access allowed');
 
-class User extends CI_Controller {
-
+class User extends CI_Controller 
+{
+	//Koneksi Model
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("User_model");
+        $this->load->library('form_validation');
+    }
+	//Read
 	public function index()
 	{
+		$data['User'] = $this->User_model->getAll();
 		$this->load->view('admin/header');
-		$this->load->view('admin/user/view_user');
+		$this->load->view('admin/user/view_user',$data);
 		$this->load->view('admin/footer');
 	}
-	
 
+	//Update/Edit(tambahan database poin)
+	public function edit($id)
+	{
+		$data['User'] = $this->User_model->getById($id);
+		$this->load->view('admin/header');
+		$this->load->view('admin/User/view_User_edit',$data);
+		$this->load->view('admin/footer');
+	}
+	public function update()
+	{
+		$this->form_validation->set_rules('nama','Nama','required');
+		$this->form_validation->set_rules('email','Email','required');
+		$this->form_validation->set_rules('password','Password');
+		$this->form_validation->set_rules('level','Level','required');
+		$this->form_validation->set_rules('poin','Poin','required');
+		if ($this->form_validation->run()==true)
+        {
+		 	$id = $this->input->post('id');
+			$data['nama'] = $this->input->post('nama');
+			$data['email'] = $this->input->post('email');
+			$data['password'] = $this->input->post('password');
+			$data['level'] = $this->input->post('level');
+			$data['poin'] = $this->input->post('poin');
+			$this->User_model->update($data,$id);
+			redirect('admin/User');
+		}
+		else
+		{
+			$id = $this->input->post('id');
+			$data['User'] = $this->User_model->getById($id);
+			$this->load->view('admin/header');
+			$this->load->view('admin/User/view_user_edit',$data);
+			$this->load->view('admin/footer');
+		}
+	}
+	//Delete
+	public function delete($id)
+	{
+		$this->User_model->delete($id);
+		redirect('admin/User');
+	}
+	
+	
 }

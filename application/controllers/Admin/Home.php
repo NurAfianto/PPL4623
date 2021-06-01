f357e971d64c724830bfa6fc6b1b9eb7fad346d4
NurAfianto <61892816+NurAfianto@users.noreply.github.com>
2021-06-01 13:55:27 +0700

    Add files via upload
    update

--- application/controllers/Admin/Home.php
+++ application/controllers/Admin/Home.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	function __construct() {
+		parent::__construct();
+		check_not_login();
+		check_user();
+	}
+
 	public function index()
 	{
 

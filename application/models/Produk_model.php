e8122e20669720b8983a6c8fe38c3b4dd2121d42
joms44 <59303795+joms44@users.noreply.github.com>
2021-06-03 21:27:13 +0700

    Add files via upload

---
+++ application/models/Produk_model.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Produk_model extends CI_Model {
+
+	public function get(){
+		$this->db->from('tb_produk');
+		$query = $this->db->get();
+        return $query;
+	}
+
+	public function add($post){
+		$params = [
+			'nama_produk' => $post['nama_produk'],
+			'id_kategori' => $post['kategori'],
+			'harga' => $post['harga'],
+			'poin' => $post['poin'],
+			'gambar' => $post['image'],
+		];
+		$this->db->insert('tb_produk', $params);
+	}
+
+	public function edit($post){
+		// $params = [
+		// 	'nama_produk' => $post['nama_produk'],
+		// 	'id_kategori' => $post['kategori'],
+		// 	'harga' => $post['harga'],
+		// 	'poin' => $post['poin'],
+			
+		// ];
+		// if($post['image'] != null ){
+		// 	$params['image'] = $post['image'];
+		// }
+		// $this->db->where('id_produk', $post['id_produk']);
+		// $this->db->insert('tb_produk', $params);
+
+	}
+}

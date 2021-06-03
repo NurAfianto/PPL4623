690059e73e3b38ec7a55fec80e40fa1d870b969e
joms44 <59303795+joms44@users.noreply.github.com>
2021-06-03 21:26:34 +0700

    Add files via upload

--- application/controllers/Admin/Produk.php
+++ application/controllers/Admin/Produk.php
@@ -5,21 +5,66 @@ class Produk extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->model('Produk_model');
+		$data['data']=$this->Produk_model->get()->result();
+		// print_r($data['data']);
 		$this->load->view('admin/header');
-		$this->load->view('admin/produk/produk');
+		$this->load->view('admin/produk/produk',$data);
 		$this->load->view('admin/footer');
+		
 	}
 	
 	public function add(){
-		$this->load->view('admin/header');
-		$this->load->view('admin/produk/produk_add');
-		$this->load->view('admin/footer');
+		$post = $this->input->post(NULL, true);
+		if(isset($post['submit'])){
+			$config['upload_path']		= "./upload/product/";
+			$config['allowed_types']	= "gif|jpg|png|jpeg";
+			$config['max_size']			= 2048;
+			$config['file_name']		= 'produk-'.date('ymd').'-'.substr(md5(rand()),0,10);
+			$this->load->library('upload', $config);
+
+			if(@$_FILES['image']['name'] != null){
+				if($this->upload->do_upload('image')){
+					$post['image'] = $this->upload->data('file_name');
+					$this->load->model('Produk_model');
+					$this->Produk_model->add($post);
+					redirect('admin/produk');
+	
+				}else{
+					echo "gagal upload";
+				}
+			}
+			
+		}
+		else{
+			$this->load->view('admin/header');
+			$this->load->view('admin/produk/produk_add');
+			$this->load->view('admin/footer');
+		}
 	}
 
 	public function edit(){
-		$this->load->view('admin/header');
-		$this->load->view('admin/produk/produk_edit');
-		$this->load->view('admin/footer');
+		$post = $this->input->post(NULL, true);
+		if(isset($post['submit'])){
+			if(@$_FILES['image']['name'] != null){
+				if($this->upload->do_upload('image')){
+					$post['image'] = $this->upload->data('file_name');
+					$this->load->model('Produk_model');
+					$this->Produk_model->edit($post);
+	
+				}else{
+					echo "gagal upload";
+				}
+			}
+
+		}
+		else{
+			$this->load->view('admin/header');
+			$this->load->view('admin/produk/produk_edit');
+			$this->load->view('admin/footer');
+		}
 	}
+
+	
 
 }

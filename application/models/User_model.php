9b11e260c2f84e625d1facdd9621a22a182d5a80
Yulius Alvin <79980153+yulius37@users.noreply.github.com>
2021-06-09 11:46:17 +0700

    Add files via upload

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,24 +2,24 @@
  
 class User_model extends CI_Model
 {
-    private $table = "tb_user";
+    private $table = "tb_user_detail";
     //Read
     public function getAll()
     {
         return $this->db->get($this->table)->result();
     }
     //Update/Edit
-    public function getById($id)
+    public function getById($id_user)
     {
-        return $this->db->get_where($this->table, ["id" => $id])->row();
+        return $this->db->get_where($this->table, ["id_user" => $id_user])->row();
     }
     public function update($data,$id)
     {
-        return $this->db->update($this->table, $data, array('id' => $id));
+        return $this->db->update($this->table, $data, array('id_user' => $id));
     }	
     //Delete
     public function delete($id)
     {
-        return $this->db->delete($this->table, array("id" => $id));
+        return $this->db->delete($this->table, array("id_user" => $id));
     }
 }

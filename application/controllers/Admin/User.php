ee76e1f5878cc5087fc167c8f63567b0e3c85737
Yulius Alvin <79980153+yulius37@users.noreply.github.com>
2021-06-09 11:44:54 +0700

    Add files via upload

--- application/controllers/Admin/User.php
+++ application/controllers/Admin/User.php
@@ -19,10 +19,10 @@ class User extends CI_Controller
 		$this->load->view('admin/footer');
 	}
 
-	//Update/Edit(tambahan database poin)
-	public function edit($id)
+	//Update/Edit
+	public function edit($id_user)
 	{
-		$data['User'] = $this->User_model->getById($id);
+		$data['User'] = $this->User_model->getById($id_user);
 		$this->load->view('admin/header');
 		$this->load->view('admin/User/view_User_edit',$data);
 		$this->load->view('admin/footer');
@@ -32,32 +32,43 @@ class User extends CI_Controller
 		$this->form_validation->set_rules('nama','Nama','required');
 		$this->form_validation->set_rules('email','Email','required');
 		$this->form_validation->set_rules('password','Password');
-		$this->form_validation->set_rules('level','Level','required');
+		$this->form_validation->set_rules('no_hp','No_Hp','required');
+		$this->form_validation->set_rules('alamat','Alamat','required');
+		$this->form_validation->set_rules('nama_rek','Nama_rek','required');
+		$this->form_validation->set_rules('recovery_code','Recovery_code');
+		$this->form_validation->set_rules('level_user','Level_user','required');
+		$this->form_validation->set_rules('poin_earned','Poin_earned','required');
 		$this->form_validation->set_rules('poin','Poin','required');
 		if ($this->form_validation->run()==true)
         {
-		 	$id = $this->input->post('id');
+		 	$id_user = $this->input->post('id_user');
 			$data['nama'] = $this->input->post('nama');
 			$data['email'] = $this->input->post('email');
 			$data['password'] = $this->input->post('password');
-			$data['level'] = $this->input->post('level');
+			$data['no_hp'] = $this->input->post('no_hp');
+			$data['alamat'] = $this->input->post('alamat');
+			$data['nama_rek'] = $this->input->post('nama_rek');
+			$data['no_rek'] = $this->input->post('no_rek');
+			$data['recovery_code'] = $this->input->post('recovery_code');
+			$data['level_user'] = $this->input->post('level_user');
+			$data['poin_earned'] = $this->input->post('poin_earned');
 			$data['poin'] = $this->input->post('poin');
-			$this->User_model->update($data,$id);
+			$this->User_model->update($data,$id_user);
 			redirect('admin/User');
 		}
 		else
 		{
-			$id = $this->input->post('id');
-			$data['User'] = $this->User_model->getById($id);
+			$id_user = $this->input->post('id_user');
+			$data['User'] = $this->User_model->getById($id_user);
 			$this->load->view('admin/header');
 			$this->load->view('admin/User/view_user_edit',$data);
 			$this->load->view('admin/footer');
 		}
 	}
 	//Delete
-	public function delete($id)
+	public function delete($id_user)
 	{
-		$this->User_model->delete($id);
+		$this->User_model->delete($id_user);
 		redirect('admin/User');
 	}
 	

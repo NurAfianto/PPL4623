fd2ad1cd33ca13617110610086dcff0272be2536
Yulius Alvin <79980153+yulius37@users.noreply.github.com>
2021-06-09 11:45:50 +0700

    Add files via upload

--- application/views/admin/user/view_user.php
+++ application/views/admin/user/view_user.php
@@ -14,26 +14,38 @@
       <th>Nama</th>
       <th>Email</th>
       <th>Password</th>
+      <th>No HP</th>
+      <th>Alamat</th>
+      <th>Nama Rekening</th>
+      <th>No Rekening</th>
+      <th>Recovery Code</th>
       <th>Level</th>
+      <th>Poin Earned</th>
       <th>Poin</th>
       <th class="text-center">Action</th>
     </tr>
     </thead>
     <?php 
-					$id = 1;
+					$id_user = 1;
 					foreach($User as $row)
 					{
 						?>
 						<tr>
-							<td><?php echo $id++; ?></td>
+							<td><?php echo $id_user++; ?></td>
 							<td><?php echo $row->nama; ?></td>
               <td><?php echo $row->email; ?></td>
               <td><?php echo $row->password; ?></td>
-              <td><?php echo $row->level; ?></td>
+              <td><?php echo $row->no_hp; ?></td>
+              <td><?php echo $row->alamat; ?></td>
+              <td><?php echo $row->nama_rek; ?></td>
+              <td><?php echo $row->no_rek; ?></td>
+              <td><?php echo $row->recovery_code; ?></td>
+              <td><?php echo $row->level_user; ?></td>
+              <td><?php echo $row->poin_earned; ?></td>
               <td><?php echo $row->poin; ?></td>
               <td class="text-center">
-                    <a href="<?php echo base_url(); ?>admin/user/edit/<?php echo $row->id; ?>" class="btn btn-info btn-sm"><i class="far fa-edit"></i> Edit</a>
-                    <a href="<?php echo base_url(); ?>admin/user/delete/<?php echo $row->id; ?>"class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda Yakin ingin Menghapus ?')"><i class="far fa-trash-alt"></i> Hapus</a>
+                    <a href="<?php echo base_url(); ?>admin/user/edit/<?php echo $row->id_user; ?>" class="btn btn-info btn-sm"><i class="far fa-edit"></i> Edit</a>
+                    <a href="<?php echo base_url(); ?>admin/user/delete/<?php echo $row->id_user; ?>"class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda Yakin ingin Menghapus ?')"><i class="far fa-trash-alt"></i> Hapus</a>
               </td>
 						</tr>
 						<?php
--- application/views/admin/user/view_user_edit.php
+++ application/views/admin/user/view_user_edit.php
@@ -19,7 +19,7 @@
       <a href="<?=site_url('admin/user')?>" class="btn btn-warning btn-md"><i class="fas fa-arrow-left"></i> Back</a>
       <form method="post" action="<?php echo base_url(); ?>admin/user/update">
         <div class="col-md-6 mx-auto mt-4">
-        <input type="hidden" name="id" id="id" value="<?php echo $User->id; ?>"/>
+        <input type="hidden" name="id_user" id="id_user" value="<?php echo $User->id_user; ?>"/>
           <div class="form-group">
               <label for="nama">Nama user</label>
               <input type="text" name="nama" class="form-control" placeholder="Nama user" value="<?php echo $User->nama; ?>" >
@@ -35,9 +35,39 @@
               placeholder="Password user" value="<?php echo $User->password; ?>" >
           </div>
           <div class="form-group">
-              <label for="level">Level user</label>
-              <input type="text" name="level" class="form-control" 
-              placeholder="Level user" value="<?php echo $User->level; ?>" >
+              <label for="no_hp">Nomer HP</label>
+              <input type="text" name="no_hp" class="form-control" 
+              placeholder="Nomer HP" value="<?php echo $User->no_hp; ?>" >
+          </div>
+          <div class="form-group">
+              <label for="alamat">Alamat user</label>
+              <input type="text" name="alamat" class="form-control" 
+              placeholder="Alamat User" value="<?php echo $User->alamat; ?>" >
+          </div>
+          <div class="form-group">
+              <label for="nama_rek">Nama Rekening</label>
+              <input type="text" name="nama_rek" class="form-control" 
+              placeholder="Nama Rekening" value="<?php echo $User->nama_rek; ?>" >
+          </div>
+          <div class="form-group">
+              <label for="no_rek">Nomer Rekening</label>
+              <input type="text" name="no_rek" class="form-control" 
+              placeholder="Nomer Rekening" value="<?php echo $User->no_rek; ?>" >
+          </div>
+          <div class="form-group">
+              <label for="recovery_code">Recovery Code</label>
+              <input type="text" name="recovery_code" class="form-control" 
+              placeholder="Recovery Code" value="<?php echo $User->recovery_code; ?>" >
+          </div>
+          <div class="form-group">
+              <label for="level_user">Level user</label>
+              <input type="text" name="level_user" class="form-control" 
+              placeholder="Level user" value="<?php echo $User->level_user; ?>" >
+          </div>
+          <div class="form-group">
+              <label for="poin_earned">Poin Earned</label>
+              <input type="text" name="poin_earned" class="form-control" 
+              placeholder="Poin Earned" value="<?php echo $User->poin_earned; ?>" >
           </div>
           <div class="form-group">
               <label for="poin">Poin user</label>

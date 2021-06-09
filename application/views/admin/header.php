dd6bed6f13803026ef74eb648de72abadab252b9
NurAfianto <61892816+NurAfianto@users.noreply.github.com>
2021-06-09 20:47:13 +0700

    Add files via upload

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -92,7 +92,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="<?=site_url('admin/home')?>" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
@@ -100,7 +100,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?=site_url('admin/produk')?>" class="nav-link">
+            <a href="<?=site_url('admin/user')?>" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 User

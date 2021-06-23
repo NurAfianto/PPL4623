b342a02034f95ff4b7d242da88a62665655242dd
noBrainCode <62835650+noBrainCode@users.noreply.github.com>
2021-06-23 21:12:00 +0700

    Add files via upload

---
+++ application/controllers/User/Cart.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cart extends CI_Controller {
+	public function min_qty($id,$qty){
+		$data = array(
+            'rowid' => $id,
+            'qty'   => $qty-1,
+        );
+		
+		$this->cart->update($data);
+		redirect('user/product/detail_cart');
+	}
+
+    public function plus_qty($id,$qty){
+        $data = array(
+            'rowid' => $id,
+            'qty'   => $qty+1,
+        );
+		
+		$this->cart->update($data);
+		redirect('user/product/detail_cart');
+    }
+
+    public function del($id){
+        $data = array(
+            'rowid' => $id,
+            'qty'   => 0
+        );
+        
+        $this->cart->update($data);
+		redirect('user/product/detail_cart');
+    }
+
+    public function checkout(){
+        $isprocessed = $this->Invoice_model->index();
+        if($isprocessed){
+            $this->cart->destroy();
+            redirect('user/dashboard');
+        }else{
+            echo"Maaf, Pesanan Anda Gagal diproses";
+        }
+    }
+}

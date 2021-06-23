d83b6defe02945869c5118d0d443115f11d73148
joms44 <59303795+joms44@users.noreply.github.com>
2021-06-23 21:14:11 +0700

    Add files via upload

--- application/controllers/Admin/Produk.php
+++ application/controllers/Admin/Produk.php
@@ -34,6 +34,7 @@ class Produk extends CI_Controller {
 					echo "gagal upload";
 				}
 			}
+			redirect('admin/produk');
 			
 		}
 		else{
@@ -45,24 +46,41 @@ class Produk extends CI_Controller {
 		}
 	}
 
-	public function edit(){
+	public function edit($id=NULL){
 		$post = $this->input->post(NULL, true);
+		$this->load->model('Produk_model');
 		if(isset($post['submit'])){
+		$config['upload_path']		= "./upload/product/";
+		$config['allowed_types']	= "gif|jpg|png|jpeg";
+		$config['max_size']			= 2048;
+		$config['file_name']		= 'produk-'.date('ymd').'-'.substr(md5(rand()),0,10);
+		$this->load->library('upload', $config);
+
+		//echo ($post['image'] == null);
 			if(@$_FILES['image']['name'] != null){
 				if($this->upload->do_upload('image')){
+					$gambar = $this->Produk_model->getById($post['id_produk'])->row()->gambar;
+					$path = 'upload/product/'.$gambar;
+					chmod($path, 0777);
+					unlink($path);
 					$post['image'] = $this->upload->data('file_name');
-					$this->load->model('Produk_model');
 					$this->Produk_model->edit($post);
-	
 				}else{
-					echo "gagal upload";
+					$this->Produk_model->editNoImage($post);
 				}
+			}else{
+				$this->Produk_model->editNoImage($post);
 			}
+			redirect('admin/produk');
+
 
 		}
 		else{
+			$this->load->model('kategori_model');
+			$data['kategori'] = $this->kategori_model->getAll();
+			$data['row'] = $this->Produk_model->getById($id)->row();
 			$this->load->view('admin/header');
-			$this->load->view('admin/produk/produk_edit');
+			$this->load->view('admin/produk/produk_edit',$data);
 			$this->load->view('admin/footer');
 		}
 	}

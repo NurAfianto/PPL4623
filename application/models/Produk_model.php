9ee7ed13666f1cb139b94c787290e2f565e44bde
joms44 <59303795+joms44@users.noreply.github.com>
2021-06-23 21:14:53 +0700

    Add files via upload

--- application/models/Produk_model.php
+++ application/models/Produk_model.php
@@ -10,6 +10,14 @@ class Produk_model extends CI_Model {
         return $query;
 	}
 
+	public function getById($id){
+		$this->db->from('tb_produk');
+		$this->db->join('tb_kategori', 'tb_kategori.no = tb_produk.id_kategori');
+		$this->db->where('id_produk', $id);
+		$query = $this->db->get();
+        return $query;
+	}
+
 	public function add($post){
 		$params = [
 			'nama_produk' => $post['nama_produk'],
@@ -22,18 +30,33 @@ class Produk_model extends CI_Model {
 	}
 
 	public function edit($post){
-		// $params = [
-		// 	'nama_produk' => $post['nama_produk'],
-		// 	'id_kategori' => $post['kategori'],
-		// 	'harga' => $post['harga'],
-		// 	'poin' => $post['poin'],
+		$params = [
+			'nama_produk' => $post['nama_produk'],
+			'id_kategori' => $post['kategori'],
+			'harga' => $post['harga'],
+			'poin' => $post['poin'],
+			'gambar' => $post['image']
 			
-		// ];
-		// if($post['image'] != null ){
-		// 	$params['image'] = $post['image'];
-		// }
-		// $this->db->where('id_produk', $post['id_produk']);
-		// $this->db->insert('tb_produk', $params);
-
+		];
+		if($post['gambar'] != null ){
+			$params['gambar'] = $post['image'];
+		}
+		$this->db->where('id_produk', $post['id_produk']);
+		$this->db->update('tb_produk', $params);
 	}
+
+	public function editNoImage($post){
+		$params = [
+			'nama_produk' => $post['nama_produk'],
+			'id_kategori' => $post['kategori'],
+			'harga' => $post['harga'],
+			'poin' => $post['poin'],
+			
+		];
+		
+		$this->db->where('id_produk', $post['id_produk']);
+		$this->db->update('tb_produk', $params);
+	}
+
+
 }

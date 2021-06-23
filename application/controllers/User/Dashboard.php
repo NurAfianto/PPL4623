b6b864df4ce47075fa5ba8c755f70e70e5c9d413
NurAfianto <61892816+NurAfianto@users.noreply.github.com>
2021-06-23 21:25:39 +0700

    Add files via upload

--- application/controllers/User/Dashboard.php
+++ application/controllers/User/Dashboard.php
@@ -15,6 +15,25 @@ class Dashboard extends CI_Controller {
 		$this->load->view('user/games/flush');
 	}
 
+	public function egg()
+	{
+		$this->load->view('user/games/egg');
+	}
+
+	public function egg_open()
+	{
+		$data['poin'] = rand(1,100);
+		$this->load->view('user/games/egg_open',$data);
+	}
+
+	public function games()
+	{
+		$this->load->view('user/header');
+		$this->load->view('user/games/games');
+		$this->load->view('user/footer');
+	}
+
+
 	public function cart()
 	{
 		$this->load->view('user/header');

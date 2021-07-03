49521e6993a744128409b24c59e3c79015b07cb0
NurAfianto <61892816+NurAfianto@users.noreply.github.com>
2021-07-03 18:27:38 +0700

    Add files via upload

--- application/views/user/header.php
+++ application/views/user/header.php
@@ -17,7 +17,7 @@
   <link rel="stylesheet" href="<?=base_url()?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="<?=base_url()?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="<?=base_url()?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-  <link rel="stylesheet" href="<?=base_url()?>/assets/css/style-user.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/css/style-user.css">
   </head>
 <body class="">
 <div class="" style="background-color: #F7F5F5;">

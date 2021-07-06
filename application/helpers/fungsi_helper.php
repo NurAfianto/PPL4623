3323e9fe0b46295d2030b443315775fa36a88477
NurAfianto <61892816+NurAfianto@users.noreply.github.com>
2021-07-06 19:49:48 +0700

    Add files via upload

--- application/helpers/fungsi_helper.php
+++ application/helpers/fungsi_helper.php
@@ -32,3 +32,27 @@ function print_user_detail(){
     $user = $ci->user_m->get_detail($user_name);
     return $user->row();
 }
+
+function check_up_level(){
+    $ci =& get_instance();
+    $ci->load->model('user_m');
+    $data_user = $ci->user_m->get($ci->session->userdata('userid'))->row();
+    $user_name = $ci->session->userdata('nama');
+    $user_email = $ci->session->userdata('email');
+    $user = $ci->user_m->get_details($user_name, $user_email);
+    $poin_earned = $user->row()->poin_earned;
+    $user_level = $user->row()->level_user;
+    $post['id_user'] = $user->row()->id_user;
+
+    if($poin_earned >= 0 && $poin_earned <500 && $user_level != 'Bronze'){
+        $post['level_user'] = 'Bronze';
+    }else if($poin_earned >= 500 && $poin_earned <1000 && $user_level != 'Silver'){
+        $post['level_user'] = 'Silver';
+    }else if($poin_earned >= 1000 && $poin_earned <2000 && $user_level != 'Gold'){
+        $post['level_user'] = 'Gold';
+    }else if($poin_earned >= 2000 && $user_level != 'Platinum'){
+        $post['level_user'] = 'Platinum';
+    }
+
+    $ci->user_m->update_level($post);
+}
